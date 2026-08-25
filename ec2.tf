78bcda0345683e4223556559155772d136da9272
Manasi Patil <116967020+Manasipatil4@users.noreply.github.com>
2026-08-25 14:32:07 +0530

    Update S3 bucket name in ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 resource "aws_s3_object" "index" {
-  bucket = "manasi-bucket"
+  bucket = "manasi-patil-b73"
   key    = "index.html"
   source = "/root/index.html"
 }
