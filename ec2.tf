89faf9cd82438959b61aaba641cb2cd867955b4e
Manasi Patil <116967020+Manasipatil4@users.noreply.github.com>
2026-08-25 14:30:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,8 @@
 provider "aws" {
-    region = "eu-north-1"  
+  region = "eu-north-1"
 }
-resource "aws_instance" "ec2" {
-    ami = "ami-0aba19e56f3eaec05"
-    instance_type = "t3.micro"
-    key_name = "manasi"
-
-    tags = {
-      Name = "my_ec2"
-    } 
+resource "aws_s3_object" "index" {
+  bucket = "manasi-bucket"
+  key    = "index.html"
+  source = "/root/index.html"
 }
