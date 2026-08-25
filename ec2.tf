044d487f07599bb3f6f72693d11f75e601df68b0
Manasi Patil <116967020+Manasipatil4@users.noreply.github.com>
2026-08-25 15:21:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,18 @@
 resource "null_resource" "sync_website" {
 
   provisioner "local-exec" {
-    command = "aws s3 sync . s3://manasi-patil-b73/ --exclude '*' --include 'index.html'"
+    command = <<-EOT
+      echo "Terraform working directory:"
+      pwd
+
+      echo "Files in Jenkins workspace:"
+      ls -la
+
+      echo "Syncing index.html to S3:"
+      aws s3 sync . s3://manasi-patil-b73/ --exclude "*" --include "index.html"
+
+      echo "S3 bucket contents:"
+      aws s3 ls s3://manasi-patil-b73/
+    EOT
   }
 }
